b245bd1eb9467cdb18ed2b61d72bccf7d1665f92
Dmitry Golubkov <master@hsdesign.ru>
2020-06-02 18:57:21 +0300

    Trying to add cedrus264 video encoder.

--- libavcodec/cedrus264.c
+++ libavcodec/cedrus264.c
@@ -287,7 +287,7 @@ static int cedrus264_encode(AVCodecContext *avctx, AVPacket *pkt,
 	int result;
 
 	/* Copy data */
-	result = avpicture_layout((const AVPicture *)frame, PIX_FMT_NV12,
+	result = avpicture_layout((const AVPicture *)frame, AV_PIX_FMT_NV12,
 		avctx->width, avctx->height, c4->input_buf->virt, c4->frame_size);
  	if(result < 0){
 		av_log(avctx, AV_LOG_ERROR, "Input buffer too small.\n");
@@ -333,7 +333,7 @@ static int cedrus264_encode(AVCodecContext *avctx, AVPacket *pkt,
 
 	size = readl(c4->ve_regs + VE_AVC_VLE_LENGTH) / 8;
 	if(size > 0){
-		if ((result = ff_alloc_packet2(avctx, pkt, size)) < 0){
+		if ((result = ff_alloc_packet2(avctx, pkt, size, 0)) < 0){
 			av_log(avctx, AV_LOG_ERROR, "Packet allocation error.\n");
 			return result;
 		}
@@ -398,5 +398,5 @@ AVCodec ff_cedrus264_encoder = {
 	.init           = cedrus264_encode_init,
 	.encode2        = cedrus264_encode,
 	.close          = cedrus264_close,
-	.priv_class	= &cedrus264_class,
+	.priv_class     = &cedrus264_class,
 };
